56f0dbbb0b2695865228f71adebe2b3767f71efe
Chris DuPuis <cpdupuis@gmail.com>
2018-10-11 17:59:34 -0700

    Added documentation to RobotDrivingInterface.

--- learningrobot/src/main/java/com/bitsforabetterworld/learningrobot/RobotDrivingInterface.java
+++ learningrobot/src/main/java/com/bitsforabetterworld/learningrobot/RobotDrivingInterface.java
@@ -1,5 +1,8 @@
 package com.bitsforabetterworld.learningrobot;
 
 public interface RobotDrivingInterface {
-    public void setSpeed(double xSpeed, double ySpeed, double rotation);
+    public void setSpeed(
+        double forwardSpeed /* m/s, with negative being backwards*/, 
+        double rightSpeed /* m/s, with negative being leftwards*/,
+        double rotationSpeed /* degrees/second, with negative being counterclockwise*/);
 }
